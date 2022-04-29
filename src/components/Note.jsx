812a535b1d41b8d7e05e12c9e4609820fbf9ff62
Akshat Zala <akshatzala@gmail.com>
2022-04-29 23:27:29 +0530

    Initial Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div>
+            <b>This is the note title</b>
+            <p>This is note content</p>
+        </div>
+    )
+}
+export default Note;
